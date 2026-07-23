42f05fd6be458e3c22e0005196db28ffb829ef49
song jinmyoung <sjinm1222@gmail.com>
2026-07-23 10:16:56 +0900

    [프로그래머스] L1 - 짝수와 홀수 (#224)

---
+++ programmers/L1_입문/224_짝수와_홀수.py
@@ -0,0 +1,12 @@
+# 짝수와 홀수
+# 프로그래머스 L1 (입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12937
+# 알고리즘: 기본연산, 조건문
+# 작성자: 학생
+# 작성일: 2026. 07. 23. 10:16:55
+
+def solution(num):
+    if num%2==0:
+        return 'Even'
+    else:
+        return 'Odd'
