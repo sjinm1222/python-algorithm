bfabc9af58fa13a10aec5fe4ac4ae2523099777b
song jinmyoung <sjinm1222@gmail.com>
2026-07-23 10:17:38 +0900

    [프로그래머스] L1 - 평균 구하기 (#225)

---
+++ programmers/L1_입문/225_평균_구하기.py
@@ -0,0 +1,10 @@
+# 평균 구하기
+# 프로그래머스 L1 (입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12944
+# 알고리즘: 기본연산, 배열
+# 작성자: 학생
+# 작성일: 2026. 07. 23. 10:17:37
+
+def solution(arr):
+    answer = sum(arr)/len(arr)
+    return answer
