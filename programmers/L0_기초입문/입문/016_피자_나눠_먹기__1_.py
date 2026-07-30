130da20ec494cdae84fbc97db4255bd4c1d5595f
song jinmyoung <sjinm1222@gmail.com>
2026-07-30 09:20:24 +0900

    [프로그래머스] L0 - 피자 나눠 먹기 (1) (#16)

--- programmers/L0_기초입문/입문/016_피자_나눠_먹기__1_.py
+++ programmers/L0_기초입문/입문/016_피자_나눠_먹기__1_.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120814
 # 알고리즘: 기초
 # 작성자: 학생
-# 작성일: 2026. 07. 30. 09:20:19
+# 작성일: 2026. 07. 30. 09:20:24
 
 def solution(n):
     import math
