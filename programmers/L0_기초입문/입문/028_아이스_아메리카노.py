26a7fba4fc67f18ca3aa75cfb2c2e27a029242c2
song jinmyoung <sjinm1222@gmail.com>
2026-08-06 08:56:49 +0900

    [프로그래머스] L0 - 아이스 아메리카노 (#28)

---
+++ programmers/L0_기초입문/입문/028_아이스_아메리카노.py
@@ -0,0 +1,10 @@
+# 아이스 아메리카노
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120819
+# 알고리즘: 기초
+# 작성자: 학생
+# 작성일: 2026. 08. 06. 08:56:46
+
+def solution(money):
+    answer=[money//5500,money%5500]
+    return answer
