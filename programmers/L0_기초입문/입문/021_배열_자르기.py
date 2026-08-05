112b6b6ee1d0b6bc5c711a3a8fc5adc5c86c1b6e
song jinmyoung <sjinm1222@gmail.com>
2026-08-06 08:37:49 +0900

    [프로그래머스] L0 - 배열 자르기 (#21)

---
+++ programmers/L0_기초입문/입문/021_배열_자르기.py
@@ -0,0 +1,10 @@
+# 배열 자르기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120833
+# 알고리즘: 기초
+# 작성자: 학생
+# 작성일: 2026. 08. 06. 08:37:46
+
+def solution(numbers, num1, num2):
+    answer = numbers[num1:num2+1]
+    return answer
