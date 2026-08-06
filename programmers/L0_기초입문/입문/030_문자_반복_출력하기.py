af112b64cca74c5489ebfa81b01f28a0feac80f3
song jinmyoung <sjinm1222@gmail.com>
2026-08-06 09:24:11 +0900

    [프로그래머스] L0 - 문자 반복 출력하기 (#30)

---
+++ programmers/L0_기초입문/입문/030_문자_반복_출력하기.py
@@ -0,0 +1,10 @@
+# 문자 반복 출력하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120825
+# 알고리즘: 기초
+# 작성자: 학생
+# 작성일: 2026. 08. 06. 09:24:08
+
+def solution(my_string, n):
+    answer = ''.join([i*n for i in my_string])
+    return answer
